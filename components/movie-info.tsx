6e07cbe06129b47bd80c4d6cc9d54f0453328cae
jaehee-bae <hjaeb7777@gmail.com>
2024-03-30 13:12:30 +0900

    chore: potato -> styles로 수정

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,5 @@
 import { API_URL } from "../app/constants";
-import potato from "../styles/movie-info.module.css";
+import styles from "../styles/movie-info.module.css";
 
 export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
@@ -9,14 +9,14 @@ export async function getMovie(id: string) {
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
   return (
-    <div className={potato.container}>
+    <div className={styles.container}>
       <img
         src={movie.poster_path}
-        className={potato.poster}
+        className={styles.poster}
         alt={movie.title}
       />
-      <div className={potato.info}>
-        <h1 className={potato.title}>{movie.title}</h1>
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
         <h3>⭐️ {movie.vote_average.toFixed()}</h3>
         <p>{movie.overview}</p>
         <a href={movie.homepage} target={"_blank"}>
